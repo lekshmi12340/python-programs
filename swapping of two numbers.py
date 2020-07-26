d92891aef61438b9e236be68e49d955fb3a30b78
lekshmi12340 <60183194+lekshmi12340@users.noreply.github.com>
2020-07-26 12:44:35 +0530

    Create swapping of two numbers.py

---
+++ swapping of two numbers.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print("The first number is ",num1 ,"Second number is ",num2)
